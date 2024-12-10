6f0de5138d7a0e97613178945dfcbcd1a3e8a319
shubhamiscodding <shubham.modi.cg@gmail.com>
2024-12-10 11:39:37 +0530

    added data through fetch api in you tube MainPage

--- you-vite/src/componant/MainPage.jsx
+++ you-vite/src/componant/MainPage.jsx
@@ -1,59 +1,25 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 function MainPage(){
-    let vedio=[
-        {id:1,
-            url:"https://raw.githubusercontent.com/PatelNeelMahesh/frontend_tasks/refs/heads/main/02.youtube-clone/assets/Thumbnail-8.png",
-            url2:"https://raw.githubusercontent.com/PatelNeelMahesh/frontend_tasks/refs/heads/main/02.youtube-clone/assets/Ellipse%204%20(1).png",
-            dis:"Bahubali | coke studio bangal|seson one | ritu X nandita", channel:"coke studio bangal",view:"1.5mil", days:"2 days ago"},
-        {id:2,
-            url:"https://raw.githubusercontent.com/PatelNeelMahesh/frontend_tasks/refs/heads/main/02.youtube-clone/assets/Thumbnail-9.png",
-            url2:"https://raw.githubusercontent.com/PatelNeelMahesh/frontend_tasks/refs/heads/main/02.youtube-clone/assets/Ellipse%204%20(1).png",
-            dis:"Bahubali | coke studio bangal|seson one | ritu X nandita", channel:"coke studio bangal",view:"1.5mil", days:"2 days ago"},
-        {id:3,
-            url:"https://raw.githubusercontent.com/PatelNeelMahesh/frontend_tasks/refs/heads/main/02.youtube-clone/assets/Thumbnail.png",
-            url2:"https://raw.githubusercontent.com/PatelNeelMahesh/frontend_tasks/refs/heads/main/02.youtube-clone/assets/Ellipse%204%20(1).png",
-            dis:"Bahubali | coke studio bangal|seson one | ritu X nandita", channel:"coke studio bangal",view:"1.5mil", days:"2 days ago"},
-        {id:4,
-            url:"https://raw.githubusercontent.com/PatelNeelMahesh/frontend_tasks/refs/heads/main/02.youtube-clone/assets/Thumbnail-7.png",
-            url2:"https://raw.githubusercontent.com/PatelNeelMahesh/frontend_tasks/refs/heads/main/02.youtube-clone/assets/Ellipse%204%20(1).png",
-            dis:"Bahubali | coke studio bangal|seson one | ritu X nandita", channel:"coke studio bangal",view:"1.5mil", days:"2 days ago"},
-        {id:5,
-            url:"https://raw.githubusercontent.com/PatelNeelMahesh/frontend_tasks/refs/heads/main/02.youtube-clone/assets/Thumbnail-6.png",
-            url2:"https://raw.githubusercontent.com/PatelNeelMahesh/frontend_tasks/refs/heads/main/02.youtube-clone/assets/Ellipse%204%20(1).png",
-            dis:"Bahubali | coke studio bangal|seson one | ritu X nandita", channel:"coke studio bangal",view:"1.5mil", days:"2 days ago"},
-        {id:6,
-            url:"https://raw.githubusercontent.com/PatelNeelMahesh/frontend_tasks/refs/heads/main/02.youtube-clone/assets/Thumbnail-5.png",
-            url2:"https://raw.githubusercontent.com/PatelNeelMahesh/frontend_tasks/refs/heads/main/02.youtube-clone/assets/Ellipse%204%20(1).png",
-            dis:"Bahubali | coke studio bangal|seson one | ritu X nandita", channel:"coke studio bangal",view:"1.5mil", days:"2 days ago"},
-        {id:7,
-            url:"https://raw.githubusercontent.com/PatelNeelMahesh/frontend_tasks/refs/heads/main/02.youtube-clone/assets/Thumbnail-4.png",
-            url2:"https://raw.githubusercontent.com/PatelNeelMahesh/frontend_tasks/refs/heads/main/02.youtube-clone/assets/Ellipse%204%20(1).png",
-            dis:"Bahubali | coke studio bangal|seson one | ritu X nandita", channel:"coke studio bangal",view:"1.5mil", days:"2 days ago"},
-        {id:8,
-            url:"https://raw.githubusercontent.com/PatelNeelMahesh/frontend_tasks/refs/heads/main/02.youtube-clone/assets/Thumbnail-3.png",
-            url2:"https://raw.githubusercontent.com/PatelNeelMahesh/frontend_tasks/refs/heads/main/02.youtube-clone/assets/Ellipse%204%20(1).png",
-            dis:"Bahubali | coke studio bangal|seson one | ritu X nandita", channel:"coke studio bangal",view:"1.5mil", days:"2 days ago"},
-        {id:9,
-            url:"https://raw.githubusercontent.com/PatelNeelMahesh/frontend_tasks/refs/heads/main/02.youtube-clone/assets/Thumbnail-2.png",
-            url2:"https://raw.githubusercontent.com/PatelNeelMahesh/frontend_tasks/refs/heads/main/02.youtube-clone/assets/Ellipse%204%20(1).png",
-            dis:"Bahubali | coke studio bangal|seson one | ritu X nandita", channel:"coke studio bangal",view:"1.5mil", days:"2 days ago"},
-        {id:10,
-            url:"https://raw.githubusercontent.com/PatelNeelMahesh/frontend_tasks/refs/heads/main/02.youtube-clone/assets/Thumbnail-1.png",
-            url2:"https://raw.githubusercontent.com/PatelNeelMahesh/frontend_tasks/refs/heads/main/02.youtube-clone/assets/Ellipse%204%20(1).png",
-            dis:"Bahubali | coke studio bangal|seson one | ritu X nandita", channel:"coke studio bangal",view:"1.5mil", days:"2 days ago"},
-        {id:11,
-            url:"https://raw.githubusercontent.com/PatelNeelMahesh/frontend_tasks/refs/heads/main/02.youtube-clone/assets/Thumbnail-11.png",
-            url2:"https://raw.githubusercontent.com/PatelNeelMahesh/frontend_tasks/refs/heads/main/02.youtube-clone/assets/Ellipse%204%20(1).png",
-            dis:"Bahubali | coke studio bangal|seson one | ritu X nandita", channel:"coke studio bangal",view:"1.5mil", days:"2 days ago"},
-        {id:12,
-            url:"https://raw.githubusercontent.com/PatelNeelMahesh/frontend_tasks/refs/heads/main/02.youtube-clone/assets/Thumbnail-10.png",
-            url2:"https://raw.githubusercontent.com/PatelNeelMahesh/frontend_tasks/refs/heads/main/02.youtube-clone/assets/Ellipse%204%20(1).png",
-            dis:"Bahubali | coke studio bangal|seson one | ritu X nandita", channel:"coke studio bangal",view:"1.5mil", days:"2 days ago"}
-        ]
+    
+    const getPosts = async () => {
+        const response = await fetch("http://localhost:3000/vedio", {
+            method : "GET"
+        });
+        return response.json();
+    }
+    const [data,setData] = useState([]);
+
+    useEffect(() => {
+        getPosts().then((Posts) => setData(Posts))
+    },[])
+
     return(
     <>
     <div className="main">
     <div className="box">
-            {vedio.map((i)=>(
+            {data.map((i)=>(
                 <div key={i.id} className="flex">
                     <img src={i.url} alt="#"></img>
                     <div className="tittle">
